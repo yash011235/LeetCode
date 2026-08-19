955ff59bd7ac06eeed6600c0079120cff321c940
yash011235 <yashasvi.mahawar.cse24@itbhu.ac.in>
2026-08-19 18:06:37 +0530

    Time: 0 ms (100%), Space: 12.5 MB (47.19%) - LeetHub

--- 0814-binary-tree-pruning/0814-binary-tree-pruning.cpp
+++ 0814-binary-tree-pruning/0814-binary-tree-pruning.cpp
@@ -1,60 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
 
-    int total(TreeNode* root){
+    bool dfs(TreeNode* root){
         if(!root){
-            return 0;
+            return false;
         }
-        return 1 + total(root->left) + total(root->right);
-    }
-
-    int solve(unordered_map<int, int>&sums, TreeNode* root, int i){
-        if(!root){
-            return 0;
+        bool left = dfs(root->left);
+        bool right = dfs(root->right);
+        if(!left){
+            root->left = NULL;
         }
-        int left = solve(sums, root->left, 2 * i + 1);
-        int right = solve(sums, root->right, 2 * i + 2);
-
-        sums[i] = left + right + (root->val);
-        return sums[i];
-    }
-
-    TreeNode* prune(TreeNode* root, unordered_map<int, int>&sums, int i){
-        if(!root){
-            return NULL;
+        if(!right){
+            root->right = NULL;
         }
-        if(!sums.count(i)){
-            return NULL;
-        }
-        root->left = prune(root->left, sums, 2 * i + 1);
-        root->right = prune(root->right, sums, 2 * i + 2);
-        if(sums[i] == 0){
-            return NULL;
-        }
-        else{
-            return root;
-        }
+        return (root->val) || left || right;
     }
 
     TreeNode* pruneTree(TreeNode* root) {
-        int totalNodes = total(root);
-        unordered_map<int, int>sums;
-        solve(sums, root, 0);
-        if(sums[0] == 0){
+        if(!dfs(root)){
             return NULL;
         }
-        prune(root, sums, 0);
         return root;
     }
 };
