8139f90cdffdcb64f3dd225ced145de109243875
yash011235 <yashasvi.mahawar.cse24@itbhu.ac.in>
2026-08-20 11:01:05 +0530

    Time: 4 ms (2.45%), Space: 15.5 MB (13.57%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -11,39 +11,39 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root, vector<int>&ans){
+    void solve(TreeNode* root, vector<int>&leaves){
         if(!root){
             return;
         }
-        if(root->left == NULL && root->right == NULL){
-            ans.push_back(root->val);
+        solve(root->left, leaves);
+        solve(root->right, leaves);
+        if(!root->left && !root->right){
+            leaves.push_back(root->val);
         }
-        solve(root->left, ans);
-        solve(root->right, ans);
     }
-
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
         vector<int>leaves1;
+        vector<int>leaves2;
         solve(root1->left, leaves1);
         solve(root1->right, leaves1);
-        if(root1->left == NULL && root1->right == NULL){
+        if(leaves1.empty()){
             leaves1.push_back(root1->val);
         }
-        vector<int>leaves2;
         solve(root2->left, leaves2);
         solve(root2->right, leaves2);
-        if(root2->left == NULL && root2->right == NULL){
+        if(leaves2.empty()){
             leaves2.push_back(root2->val);
         }
-        int n1 = leaves1.size();
-        int n2 = leaves2.size();
-        if(n1 != n2){
+        int n = leaves1.size();
+        int m = leaves2.size();
+        if(n != m){
             return false;
         }
-        for(int i = 0; i < n1; i++){
+        for(int i = 0; i < n; i++){
             if(leaves1[i] != leaves2[i]){
                 return false;
             }
+            cout << leaves1[i] << " " << leaves2[i] << "\n";
         }
         return true;
     }
